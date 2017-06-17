de5c8e23e84d8954f8ee68cd432932929fab5864
abe- <info@croopier.com>
2017-06-17 17:20:15 +0200

    parcelas

--- docs/graficos/parcelas.js
+++ docs/graficos/parcelas.js
@@ -25,8 +25,9 @@ function preload() {
 function setup() {
   pg = createGraphics(800, 519);
   var as = 519. / 800.;
-  var w = document.innerWidth;
-  var h = document.innerHeight*as;
+  var zoom = .8;
+  var w = document.innerWidth*zoom;
+  var h = document.innerHeight*as*zoom;
 
   createCanvas(w,h);
   nombres = new Array("Norte","Duero","Ebro","Pirineo","Jucar","Tajo","Guadiana","Guadalquivir","Sur", "Segura", "Baleares y Canarias");
