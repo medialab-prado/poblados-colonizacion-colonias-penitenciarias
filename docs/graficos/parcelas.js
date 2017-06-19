a02051d47eefca65e536c1f35e606712e9c08bcd
abe- <info@croopier.com>
2017-06-19 09:55:53 +0200

    parcelas

--- docs/graficos/parcelas.js
+++ docs/graficos/parcelas.js
@@ -42,7 +42,7 @@ function draw() {
   }
 
   // Parcelas segun PGC
-  var mc = cmap.get(mx,my);
+  var mc = cmap.get(mouseX,mouseY);
   for (var n = 0; n < colores.length; n++) {
     if (compara(mc,color(colores[n]))) {
       tint(255,100);
